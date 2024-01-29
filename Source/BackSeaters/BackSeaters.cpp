edca3fcea30c4474e74a38c5c43379ef54e6a0df
Alejandro Daza Truque <alejodt995@gmail.com>
2024-01-28 21:29:22 -0500

    BackRowSeaters First commit

---
+++ Source/BackSeaters/BackSeaters.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BackSeaters.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BackSeaters, "BackSeaters" );
